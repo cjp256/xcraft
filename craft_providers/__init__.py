8988f051d893932cba432ee8b1f8e4df1f0dd470
Chris Patterson <chris.patterson@canonical.com>
2020-12-08 13:57:01 -0500

    Bump version: 0.2.0 → 0.0.1

--- craft_providers/__init__.py
+++ craft_providers/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "__version__ = "0.2.0""  # noqa: F401
+__version__ = "0.0.1"  # noqa: F401
 
 from .executed_provider import ExecutedProvider  # noqa: F401
 from .host import HostProvider  # noqa: F401
